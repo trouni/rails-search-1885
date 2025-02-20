a77d1a9ada5610255323d79c183285e682e89357
Trouni Tiet <trouni@gmail.com>
2025-02-20 11:13:52 +0900

    Add PgSearch implementation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,14 +2,24 @@ class MoviesController < ApplicationController
   def index
     @movies = 
       if params[:q].present?
-        sql_query = <<~SQL
-        title @@ :q OR
-        synopsis @@ :q OR
-        directors.first_name @@ :q OR
-        directors.last_name @@ :q
-        SQL
+        ## PG SEARCH IMPLEMENTATION
+        movies = Movie.search(params[:q])
 
-        Movie.joins(:director).where(sql_query, q: "%#{params[:q]}%")
+        ## Multiple filters
+        # movies = movies.near(params[:location]) if params[:location]
+        # movies = movies.where('year > ?', params[:min_year]) if params[:min_year]
+        # movies = movies.where('year < ?', params[:max_year]) if params[:max_year]
+
+        ## SQL IMPLEMENTATION
+
+        # sql_query = <<~SQL
+        # title @@ :q OR
+        # synopsis @@ :q OR
+        # directors.first_name @@ :q OR
+        # directors.last_name @@ :q
+        # SQL
+
+        # Movie.joins(:director).where(sql_query, q: "%#{params[:q]}%")
       else
         Movie.all
       end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,15 @@
 class Movie < ApplicationRecord
   belongs_to :director
+  
+  include PgSearch::Model
+  multisearchable against: [:title, :synopsis]
+
+  pg_search_scope :search,
+    against: %w[title synopsis],
+    associated_against: {
+      director: %w[first_name last_name]
+    },
+    using: { tsearch: { prefix: true } }
+
+  # pg_search_scope :search_by_director
 end
